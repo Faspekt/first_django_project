17d38ac81ac689d67e2b86ac188cd9fd0b463d82
Nikita Dudin <nikitadudin119@gmail.com>
2023-04-20 21:55:06 +0300

    add funk 'search'

--- news/views.py
+++ news/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render, redirect
 from .models import News
 from .forms import NewsForm
 from django.views.generic import DetailView, UpdateView, DeleteView
-
+from django.db.models import Q
 
 
 def news_home(request):
-    news = News.objects.order_by('-data_create')[:10]
+    search_query = request.GET.get("search", "")
+
+    if search_query:
+        news = News.objects.filter(Q(name_news__icontains=search_query)| Q(anons__icontains=search_query))
+    else:
+        news = News.objects.order_by('-data_create')[:10]
+
     data = {
         "news": "Новости",
         "all_news": news
