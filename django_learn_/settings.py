5a25a100164da569d8e48d7096867e652308d061
Nikita Dudin <nikitadudin119@gmail.com>
2023-04-25 23:06:33 +0300

    put the settings in a separate file

--- django_learn_/settings.py
+++ django_learn_/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-import os
+from os import getenv
 
 
 load_dotenv()
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,14 +38,14 @@ LOGIN_REDIRECT_URL = "/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_USER = getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = getenv('DEFAULT_FROM_EMAIL')
 
 
 # Application definition
@@ -76,7 +76,7 @@ ROOT_URLCONF = "django_learn_.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "BACKEND": getenv("BACKEND_DB"),
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -98,12 +98,12 @@ WSGI_APPLICATION = "django_learn_.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv('NAME_DB'),
-        "USER": os.getenv('USER_DB'),
-        "PASSWORD": os.getenv('PASSWORD_DB'),
-        "HOST": os.getenv('HOST_DB'),
-        "PORT": os.getenv('PORT_DB'),
+        "ENGINE": getenv('ENGINE_DB'),
+        "NAME": getenv('NAME_DB'),
+        "USER": getenv('USER_DB'),
+        "PASSWORD": getenv('PASSWORD_DB'),
+        "HOST": getenv('HOST_DB'),
+        "PORT": getenv('PORT_DB'),
     }
 }
 
