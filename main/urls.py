4e185217e6d1a7d81dcf0e80405c908c6957f1b6
Nikita Dudin <nikitadudin119@gmail.com>
2023-04-29 23:02:57 +0300

    changed from the 'home' function to the 'HomePage' class

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 from . import views
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    re_path(r"^about", views.about, name="about"),
-    re_path(r"^help", views.help, name="help")
+    path("", views.HomePage.as_view(), name="home"),
+    re_path(r"^about_page", views.about_page, name="about"),
+    re_path(r"^help_page", views.help_page, name="help"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,24 +1,59 @@
-from django.shortcuts import render
+import time
 
 
-
-def home(request):
-    data = {
-        "title": "Главная",
-    }
-    return render(request, "main/home.html", data)
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from users.models import User
 
 
-def about(request):
-    data = {
-        "about": "О нас"
-    }
+class HomePage(ListView):
+    """Home page"""
+
+    template_name = "main/home.html"
+
+    def get(self, request):
+        self.request.session.set_expiry(0)
+
+        user = self.request.session.get("user", None)
+        user_id = self.request.user.id
+
+        data = {"title": "Главная"}
+
+        if user is not None:  # If the user in the session exists
+
+            return render(self.request, self.template_name, data)
+
+        elif user_id is None:  # If user finished session and is not loggin
+
+            return render(self.request, self.template_name, data)
+
+        else:  # Creates a user in a new session
+            user = get_object_or_404(User, pk=self.request.user.id)
+
+            last_operation_time = self.request.session[
+                "last_operation_time"
+            ] = time.time()
+
+            self.request.session["user"] = {
+                "id": user.pk,
+                "username": user.username,
+            }
+            return render(
+                self.request,
+                self.template_name,
+                {"user": user, "last_operation_time": last_operation_time},
+            )
+
+
+def about_page(request):
+    """about page"""
+    data = {"about": "О нас"}
+
     return render(request, "main/about.html", data)
 
 
-def help(request):
-    data = {
-        "help": "Поддержка"
-    }
-    return render(request, "main/help.html", data)
+def help_page(request):
+    """help page"""
+    data = {"help": "Поддержка"}
 
+    return render(request, "main/help.html", data)
