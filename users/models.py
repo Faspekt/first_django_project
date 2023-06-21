de6fba79241bce652f1848a6413e551ed67884cf
Nikita Dudin <nikitadudin119@gmail.com>
2023-06-21 16:54:41 +0300

    add in user model field avatar

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .services import Avatar
 
 
 class User(AbstractUser):
-    avatar = models.ImageField(verbose_name="avatar", default="/static/main/image/avatar-default-icon.png")
+    avatar = models.ImageField(
+        verbose_name="avatar",
+        default="/static/main/image/avatar-default-icon.png",
+        upload_to=Avatar.path_to_save,
+    )
